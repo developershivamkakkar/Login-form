37187acc9debe29ed744f6edf5b9a42c113afef3
Shivam Kakkar <officialshivamkakkar@gmail.com>
2024-02-13 14:17:33 +0530

    Implemented login functionality using fetch

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 const button = document.querySelector("button");
 
 button.addEventListener("click", function () {
-  alert("Button Clicked ");
+  // Get the username and password from the form
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  // Create an object with the data to be sent in the request body
+  const data = {
+    username: username,
+    password: password,
+  };
+
+  fetch("http://127.0.0.1:8000/api/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      const message = data.message;
+      alert(message);
+    })
+    .catch((error) => {
+      const errorMessage = error.message;
+      alert(errorMessage);
+    });
 });
